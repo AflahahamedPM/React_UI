aaa86335b5f54bc533c773ccf03799d15d92e8a3
AflahAhamed00 <aflahahamed777@gmail.com>
2024-09-10 13:02:28 +0530

    updated footer

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import facebookImage from "/facebook.png";
+import instagramImage from "/instagram.png";
+import linkedinImage from "/linkedin.png";
 
 const Courses = () => {
   return (
-    <div className="flex flex-col gap-6 cursor-pointer">
-      <h1 className="text-custom-green">Courses</h1>
-      <h1 className='text-white font-light'>Full Stack Developer and Certification courses</h1>
-        <h1 className='text-white font-light'>Bachelor of Commerce (Bcom)</h1>
-        <h1 className='text-white font-light'>BBA Computer Application</h1>
-        <h1 className='text-white font-light'>Bsc computer science -AI and Data Science </h1>
-        <h1 className='text-white font-light'>BBA Aviation Management</h1>
-    </div>
+    <>
+      <div className="flex flex-col gap-6 cursor-pointer">
+        <h1 className="text-custom-green">Courses</h1>
+        <h1 className="text-white font-light">
+          Full Stack Developer and Certification courses
+        </h1>
+        <h1 className="text-white font-light">Bachelor of Commerce (Bcom)</h1>
+        <h1 className="text-white font-light">BBA Computer Application</h1>
+        <h1 className="text-white font-light">
+          Bsc computer science -AI and Data Science{" "}
+        </h1>
+        <h1 className="text-white font-light">BBA Aviation Management</h1>
+      </div>
+      <div className="flex self-end justify-end h-12 mt-8 gap-4">
+        <button className="py-1 px-3 bg-custom-green rounded-lg">
+          <img src={facebookImage} alt="" />
+        </button>
+        <button className="py-1 px-3 bg-custom-green rounded-lg">
+          <img src={instagramImage} alt="" />
+        </button>
+        <button className="py-1 px-3 bg-custom-green rounded-lg">
+          <img src={linkedinImage} alt="" />
+        </button>
+      </div>
+    </>
   );
 };
 
